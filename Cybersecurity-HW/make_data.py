037ddc0ea2c57b3b66cff3c0a0d722bf4f93b428
kadonne <nanolike.limit@gmail.com>
2023-10-02 14:13:56 -0500

    size factor added (adjust the number of passwords)

--- Cybersecurity-HW/make_data.py
+++ Cybersecurity-HW/make_data.py
@@ -30,8 +30,7 @@ def main():
     password_file.close()
 
 def encrypt_string(hash_string):
-    sha_signature = \
-        hashlib.sha256(hash_string.encode()).hexdigest()
+    sha_signature = hashlib.sha256(hash_string.encode()).hexdigest()
     return sha_signature
 
 
