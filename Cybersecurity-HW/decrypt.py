20f3852d0b120fd3686021af296230bc1077c60f
kadonne <nanolike.limit@gmail.com>
2023-10-02 13:39:42 -0500

    removed multiprocessing

--- Cybersecurity-HW/decrypt.py
+++ Cybersecurity-HW/decrypt.py
@@ -1,5 +1,5 @@
 import hashlib
-import multiprocessing
+#import multiprocessing
 import time
 from itertools import product
 
@@ -58,13 +58,15 @@ def digits_symbols(word_list,hash_list,digits,symbols):
 	compare_hashes(hashed,hash_list)
 
 def compare_hashes(hashed,hash_list):
+	global decrypted_passwords
+
 	# sort the computed hashed dictionary from digit_symbol and symbol_digit functions
 	sorted_hashed = sorted(hashed.items(), key=lambda x:x[0])
 	# flatten the dictionary so all the keys are stored into a 1D array
 	keys = [r[0] for r in sorted_hashed]
 
 	# print out first element to see progress for the running code
-	print(sorted_hashed[0])
+	#print(sorted_hashed[0])
 	
 	for word in hash_list:
 		try:
@@ -77,12 +79,10 @@ def compare_hashes(hashed,hash_list):
 			found = False
 		# if one of the 100 hashes is present in the dictionary, write to passwords1.txt along with the original password
 		if found:
-			write_file = open('decrypted_passwords.txt','a')
-			write_file.write('%s %s\n'%(hashes_word[0],hashes_word[1]))
-			write_file.close()
+			decrypted_passwords.append('%s %s\n'%(hashes_word[0],hashes_word[1]))
 
-
-if __name__ == "__main__":
+if __name__=='__main__':
+	decrypted_passwords = list()
 
 	symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', '|', ':', ';', '[', ']', '?', '>']
 	digits = ['0','1','2','3','4','5','6','7','8','9']
@@ -99,9 +99,16 @@ if __name__ == "__main__":
 	# store all lines full of LEET SPEAK combinations that were converted from the original wordlist full of 140,000 english words with length greater than 10
 	word_list = file1.readlines()
 	file1.close()
+	symbols_digits(word_list,hash_list,digits,symbols)
+	digits_symbols(word_list,hash_list,digits,symbols)
 	#process dedicated to appending symbols followed by digits
-	multiprocessing.Process(target=symbols_digits,args=(word_list,hash_list,digits,symbols)).start()
+	#multiprocessing.Process(target=symbols_digits,args=(word_list,hash_list,digits,symbols)).start()
 	#process dedicated to appending digits followed by symbols
-	multiprocessing.Process(target=digits_symbols,args=(word_list,hash_list,digits,symbols)).start()
+	#multiprocessing.Process(target=digits_symbols,args=(word_list,hash_list,digits,symbols)).start()
+
+	write_file = open('decrypted_passwords.txt','w')
+	write_file.writelines(decrypted_passwords)
+	write_file.close()
+
 
 	print('done')
